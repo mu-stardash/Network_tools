716c6af265dc1e47c0b38957ff92fd6965d36bf8
Dasha Efimova <dashulya0301178@gmail.com>
2022-11-03 13:57:30 +0200

    Haha

--- Stepik/lab5.py
+++ Stepik/lab5.py
@@ -10,9 +10,8 @@ def Remove_Duplicates(nums):
         else:
             last = nums[i]
             nums[count] = nums[i]
-            count+= 1   
+            count+= 1
     return count
-
 
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 print(Remove_Duplicates(nums))
