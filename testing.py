6f546972d3babf0edc980c40251734fc0b17b829
Dasha Efimova <dashulya0301178@gmail.com>
2022-11-16 12:43:59 +0300

    Idk

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ def test_hello():
 class TestClassSF:
     def test_one(self):
         x = 1
-        assert x == 1 # False
+        assert x != 1 # False
     def test_two(self):
         x = 1
         assert x != 2 # True
